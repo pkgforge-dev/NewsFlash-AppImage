1e4dd04f5f866619cbe33b266745fcd259d88049
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-09 10:27:03 +0100

    Initial commit

---
+++ get-dependencies.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+echo "Installing build dependencies for sharun & AppImage integration..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel \
+	curl \
+	desktop-file-utils \
+	git \
+	libxtst \
+	wget \
+	xorg-server-xvfb \
+	zsync
+echo "Installing the app & it's dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	newsflash
+
+if [ "$ARCH" = 'x86_64' ]; then
+	echo "Installing 'libva-intel-driver' for older Intel's video HW acceleration"
+	echo "---------------------------------------------------------------"
+	pacman -Syu --noconfirm libva-intel-driver
+fi
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh libxml2-mini mesa-nano gtk4-mini gdk-pixbuf2-mini librsvg-mini opus-mini intel-media-driver-mini
+
+echo "Extracting the app version into a version file"
+echo "---------------------------------------------------------------"
+pacman -Q newsflash | awk '{print $2; exit}' > ~/version
---
+++ newsflash-appimage.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+VERSION="$(cat ~/version)"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+# Variables used by quick-sharun
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=NewsFlash-"$VERSION"-anylinux-"$ARCH".AppImage
+export DESKTOP=/usr/share/applications/io.gitlab.news_flash.NewsFlash.desktop
+export ICON=/usr/share/icons/hicolor/scalable/apps/io.gitlab.news_flash.NewsFlash.svg
+export DEPLOY_OPENGL=1
+export DEPLOY_GSTREAMER=1
+export STARTUPWMCLASS=newsflash # For Wayland, this is 'io.gitlab.news_flash.NewsFlash', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+
+# Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/newsflash
+
+## Set gsettings to save to keyfile, instead to dconf
+echo "GSETTINGS_BACKEND=keyfile" >> ./AppDir/.env
+
+# Make the AppImage with uruntime
+./quick-sharun --make-appimage
+
+# Prepare the AppImage for release
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
