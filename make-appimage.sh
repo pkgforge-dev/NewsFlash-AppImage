1e01b3d8b1a773f7abcd16dd8917bee58ad6d3c4
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:21:56 +0100

    Migrate to new generic AppImage template

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-VERSION="$(cat ~/version)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-# Variables used by quick-sharun
+ARCH=$(uname -m)
+VERSION=$(pacman -Q newsflash | awk '{print $2; exit}')
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME=NewsFlash-"$VERSION"-anylinux-"$ARCH".AppImage
-export DESKTOP=/usr/share/applications/io.gitlab.news_flash.NewsFlash.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/io.gitlab.news_flash.NewsFlash.svg
+export DESKTOP=/usr/share/applications/io.gitlab.news_flash.NewsFlash.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_GSTREAMER=1
 export STARTUPWMCLASS=newsflash # For Wayland, this is 'io.gitlab.news_flash.NewsFlash', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
@@ -27,14 +26,9 @@ else
 fi
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun /usr/bin/newsflash /usr/bin/xdg-dbus-proxy /usr/lib/gio/modules/libgiognutls.so*
+quick-sharun /usr/bin/newsflash \
+             /usr/bin/xdg-dbus-proxy \
+             /usr/lib/gio/modules/libgiognutls.so*
 
-# Make the AppImage with uruntime
-./quick-sharun --make-appimage
-
-# Prepare the AppImage for release
-mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
