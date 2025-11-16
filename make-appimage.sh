5afef4af53d2ca06c1887461c443a4d9702bb8c1
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:33:24 +0100

    Don't add self-updater hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,6 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q newsflash | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/io.gitlab.news_flash.NewsFlash.svg
 export DESKTOP=/usr/share/applications/io.gitlab.news_flash.NewsFlash.desktop
